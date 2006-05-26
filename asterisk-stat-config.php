979599ec4008d6077a4617d2842dba73105922fb
Michał 'Wolvverine' Panasiewicz <wolvverine@pld-linux.org>
2006-05-26 10:28:09 +0000

    - config in /etc
    Changed files:
        asterisk-stat-config.php -> 1.1

---
+++ asterisk-stat-config.php
@@ -0,0 +1,14 @@
+
+define ("WEBROOT", "http://domainname.com/asterisk-stat/");
+define ("FSROOT", "/var/www/html/HTTP2/Asterisk/asterisk-stat-v2/");
+
+define ("LIBDIR", FSROOT."lib/");
+
+define ("HOST", "localhost");
+define ("PORT", "5432");
+define ("USER", "postgres");
+define ("PASS", "");
+define ("DBNAME", "cdrasterisk");
+define ("DB_TYPE", "postgres"); // mysql or postgres
+
+define ("DB_TABLENAME", "cdr");
